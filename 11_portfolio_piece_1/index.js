c20df3791225df1330f40248c2003b9881feb6c0
Robyn Paulsen <149692745+Robyn011@users.noreply.github.com>
2024-03-01 12:37:00 +0200

    Add files via upload

---
+++ 11_portfolio_piece_1/index.js
@@ -0,0 +1,19 @@
+const { document } = require("postcss");
+
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link');
+
+
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    });
+});
+
+
+
